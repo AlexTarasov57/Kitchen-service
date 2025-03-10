8f5e1b001dedf5fa759cbb1553ea21d23687f785
Alex Bul <tarasov57228@gmail.com>
2025-03-10 21:34:14 +0200

    Created forms for Cook model, registration, login, and password change

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -1,7 +1,11 @@
+from django.utils.translation import gettext_lazy as _
+
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm, AuthenticationForm, UsernameField, \
+    PasswordResetForm, SetPasswordForm, PasswordChangeForm
 
-from kitchen.models import Ingredient, Dish
+from kitchen.models import Ingredient, Dish, Cook
 
 
 class DishForm(forms.ModelForm):
@@ -17,3 +21,73 @@ class DishForm(forms.ModelForm):
     class Meta:
         model = Dish
         fields = '__all__'
+
+class CookUpdateForm(UserChangeForm):
+    password = None
+
+    class Meta:
+        model = Cook
+        fields = ("username", "first_name", "last_name", "email")
+
+
+class RegistrationForm(UserCreationForm):
+    password1 = forms.CharField(
+        label=_("Password"),
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+    )
+    password2 = forms.CharField(
+        label=_("Confirm Password"),
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}),
+    )
+
+    class Meta:
+        model = Cook
+        fields = ('username', 'email',)
+
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Username'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'example@company.com'
+            })
+        }
+
+
+class LoginForm(AuthenticationForm):
+    username = UsernameField(label=_("Your Username"),
+                             widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Username"}))
+    password = forms.CharField(
+        label=_("Your Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Password"}),
+    )
+
+
+class UserPasswordResetForm(PasswordResetForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control'
+    }))
+
+
+class UserSetPasswordForm(SetPasswordForm):
+    new_password1 = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={
+        'class': 'form-control', 'placeholder': 'New Password'
+    }), label="New Password")
+    new_password2 = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={
+        'class': 'form-control', 'placeholder': 'Confirm New Password'
+    }), label="Confirm New Password")
+
+
+class UserPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={
+        'class': 'form-control', 'placeholder': 'Old Password'
+    }), label='Old Password')
+    new_password1 = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={
+        'class': 'form-control', 'placeholder': 'New Password'
+    }), label="New Password")
+    new_password2 = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={
+        'class': 'form-control', 'placeholder': 'Confirm New Password'
+    }), label="Confirm New Password")
