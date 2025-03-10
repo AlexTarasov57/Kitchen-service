7f2455fe933aa4c846310af899c80dd60ab8c910
Alex Bul <tarasov57228@gmail.com>
2025-03-10 20:57:50 +0200

    Created view methods for DishType model

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,56 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+
+from kitchen.models import Cook, Ingredient, DishType, Dish
+
 
 # Create your views here.
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_cooks = Cook.objects.count()
+    num_ingredients = Ingredient.objects.count()
+    num_dish_types = DishType.objects.count()
+    num_dish = Dish.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_ingredients": num_ingredients,
+        "num_dish_types": num_dish_types,
+        "num_dish": num_dish,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
+
+
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
+    model = DishType
+    context_object_name = "dishtype_list"
+    template_name = "kitchen/Dish_type_list.html"
+    paginate_by = 5
+
+
+class DishTypeCreateView(LoginRequiredMixin, generic.CreateView):
+    model = DishType
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:dish_type-list")
+    template_name = "kitchen/dishtype_form.html"
+
+class DishTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = DishType
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:dish_type-list")
+    template_name = "kitchen/dishtype_form.html"
+
+
+class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = DishType
+    success_url = reverse_lazy("kitchen:dish_type-list")
+    template_name = "kitchen/dishtype_delete.html"
