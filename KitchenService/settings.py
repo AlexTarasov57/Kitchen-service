59fbe2bbe943cb835e3e9f73a6fcbad3f6630c49
Alex Bul <tarasov57228@gmail.com>
2025-03-19 19:38:37 +0200

    Fixed bugs

--- KitchenService/settings.py
+++ KitchenService/settings.py
@@ -24,6 +24,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-2d^ht^r%a^dy50
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+
 ALLOWED_HOSTS = ["127.0.0.1"]
 
 
